fe423786e515c3392748f5ecbd13a817bba02a8d
Jamison Bryant <robojamison@gmail.com>
2015-06-21 11:47:52 -0400

    Fixed Angela's email address in the database

--- sql/data.sql
+++ sql/data.sql
@@ -10,6 +10,6 @@ INSERT INTO partners VALUES
 
 INSERT INTO users VALUES
   (0, 'Jamison', 'robojamison@gmail.com', '', 35, NOW(), 0),
-  (0, 'Angela', 'angelamao115@gmail.com@gmail.com', '', 40, NOW(), 0),
+  (0, 'Angela', 'angelamao115@gmail.com', '', 40, NOW(), 0),
   (0, 'John', 'johnpalladino.ufit@gmail.com', '', 20, NOW(), 0),
   (0, 'Saddia', 'saddiacheema@gmail.com', '', 0, NOW(), 0);
